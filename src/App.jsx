da67bd0d48075dc479890fd7fdae1f2d5ef8cf3e
Jessica Sturgell <sturgell01@gmail.com>
2024-06-17 11:55:51 -0400

    added user service and login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom"
 import { ApplicationViews } from "./views/ApplicationViews.jsx";
 import { Authorized } from "./views/Authorized.jsx";
+import { Login } from "./auth/Login.jsx";
 
 export const App = () => {
     return (
     <Routes>
-      <Route path="/login" element={<>TO DO</>}/>
+      <Route path="/login" element={<Login />}/>
       <Route path="/register" element={<>TO DO</>}/>
   
       <Route 
---
+++ src/auth/Login.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import { getUserByEmail } from "../services/userService.jsx"
+
+export const Login = () => {
+  const [email, set] = useState("gagehickle@email.com")
+  const navigate = useNavigate()
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    getUserByEmail(email).then((foundUsers) => {
+      if (foundUsers.length === 1) {
+        const user = foundUsers[0]
+        localStorage.setItem(
+          "reap_user",
+          JSON.stringify({
+            id: user.id,
+          })
+        )
+
+        navigate("/")
+      } else {
+        window.alert("Invalid login")
+      }
+    })
+  }
+
+  return (
+    <main>
+      <section>
+        <form onSubmit={handleLogin}>
+          <h1>reap / sow</h1>
+          <h2>Please sign in</h2>
+          <fieldset>
+            <div>
+              <input
+                type="email"
+                value={email}
+                onChange={(evt) => set(evt.target.value)}
+                placeholder="Email address"
+                required
+                autoFocus
+              />
+            </div>
+          </fieldset>
+          <fieldset>
+            <div>
+              <button type="submit">
+                Sign in
+              </button>
+            </div>
+          </fieldset>
+        </form>
+      </section>
+      <section>
+        <Link to="/register">Not a member yet?</Link>
+      </section>
+    </main>
+  )
+}
